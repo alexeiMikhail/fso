cb66fa410f25b828984e08b9e4ec3bf3c2e247ee
alexeiMikhail <leleuxc@gmail.com>
2022-12-08 15:26:14 -0600

    Fix warning from exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -45,12 +45,14 @@ const Statistics = ({ good, neutral, bad }) => {
   }
   return (
     <table>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <Total good={good} neutral={neutral} bad={bad} />
-      <Average good={good} neutral={neutral} bad={bad} />
-      <Positive good={good} neutral={neutral} bad={bad} />
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <Total good={good} neutral={neutral} bad={bad} />
+        <Average good={good} neutral={neutral} bad={bad} />
+        <Positive good={good} neutral={neutral} bad={bad} />
+      </tbody>
     </table>
   )
 }
