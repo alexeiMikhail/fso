6ef3878da3b3f9bfbf425bbe6cfc265225761f17
alexeiMikhail <leleuxc@gmail.com>
2022-12-09 16:20:54 -0600

    Complete exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState, useRef } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameInput = (event) => {
     console.log(event.target.value)
@@ -33,9 +37,16 @@ const App = () => {
     setNewNum('')
   }
 
+  const handleFilter = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input type="text" onChange={handleFilter} value={newFilter}/></div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input autoFocus onChange={handleNameInput} ref={inputRef} value={newName}/>
@@ -47,7 +58,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
+        {persons.filter(p => p.name.toLowerCase().includes(newFilter.toLowerCase())).map(p => <p key={p.name}>{p.name} {p.number}</p>)}
       </div>
     </div>
   )
